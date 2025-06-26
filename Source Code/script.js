fde13f88a25311a636bbd29748acaa344d573441
AICOMp <ac1423004@gmail.com>
2025-06-26 10:08:15 +0800

    Add files via upload

---
+++ Source Code/script.js
@@ -0,0 +1,5 @@
+// Toggle Mobile Menu
+document.getElementById('menu-toggle').addEventListener('click', function () {
+  const menu = document.getElementById('mobile-menu');
+  menu.classList.toggle('hidden');
+});
